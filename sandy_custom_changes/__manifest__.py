6b112d2fa136cab05a9d752edc6d11b9f15d21b2
Santhosh6368611 <santhoshkumar@processdrive.com>
2025-12-17 23:01:57 +0530

    added groups for the purchase

---
+++ sandy_custom_changes/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Sandy Custom Changes',
+    'version': '1.0',
+    'description': 'Custom changes for Sandy',
+    'summary': '',
+    'author': '',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': '',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'security/purchase_groups.xml'
+    ],
+    'demo': [
+        
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
