3e6a3cdb1be9262893e057f5746ddc8c0d4a0dea
Santhosh6368611 <santhoshkumar@processdrive.com>
2025-12-19 23:32:36 +0530

    develped the approval groups and flow for the purchase approve

--- sandy_custom_changes/models/purchase_order.py
+++ sandy_custom_changes/models/purchase_order.py
@@ -1,9 +1,22 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo import SUPERUSER_ID
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    state = fields.Selection([
+        ('draft','Draft'),
+        ('sent','RFQ sent'),
+        ('to_approve','To Approve'),
+        ('purchase','Purchase Order'),
+        ('waiting', 'Waiting for Approval'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('cancel','Cancelled'),
+    ],)
+
+
 
     vendor_ids = fields.Many2many(
         'res.partner',
@@ -16,7 +29,8 @@ class PurchaseOrder(models.Model):
 
     purchase_type = fields.Selection([
         ('internal', 'Internal Procurement'),
-        ('external', 'Internal Procurement'),
+        ('external', 'External Procurement'),
+
       
     ],
     string="Purchase Type",
@@ -56,44 +70,166 @@ class PurchaseOrder(models.Model):
 
         return res
 
-    def button_confirm(self):
+
+
+    @api.model
+    def create(self, vals):
+       #  Create original RFQ
+       order = super(PurchaseOrder, self).create(vals)
+       # Always keep RFQ state
+       order.state = 'draft'
+       #  If no vendors selected → nothing else to do
+       if not order.vendor_ids:
+           return order
+       #  Create duplicate RFQs for each vendor
+       for vendor in order.vendor_ids:
+           rfq_vals = {
+               'partner_id': vendor.id,
+               'origin': order.name,
+               'state': 'draft',
+               'order_line': [],
+           }
+           # Copy order lines
+           for line in order.order_line:
+               rfq_vals['order_line'].append((0, 0, {
+                   'product_id': line.product_id.id,
+                   'name': line.name,
+                   'product_qty': line.product_qty,
+                   'price_unit': line.price_unit,
+                   'product_uom_id': line.product_uom_id.id,
+                   'date_planned': line.date_planned,
+                   'discount': line.discount,
+                   'tax_ids': [(6, 0, line.tax_ids.ids)],
+               }))
+           super(PurchaseOrder, self).create(rfq_vals)
+       return order
+
+
+
+    def action_send_for_approval(self):
         for order in self:
 
-            # Normal behavior if no multi-vendor selected
-            if not order.vendor_ids:
-                return super(PurchaseOrder, self).button_confirm()
+            # 1. Vendor must be selected
+            if not order.partner_id:
+                raise ValidationError("Please select a Vendor before sending for approval.")
 
-            # Prevent duplicate generation
-            if order.state != 'draft':
-                continue
+            # 2. Products must be added
+            if not order.order_line:
+                raise ValidationError("Add at least one product before sending for approval.")
 
-            # 1️⃣ Confirm ORIGINAL PO
-            res = super(PurchaseOrder, order).button_confirm()
+            # 3. Amount must be greater than zero
+            if order.amount_total <= 0:
+                raise ValidationError("Total amount must be greater than 0 before approval.")
 
-            # 2️⃣ Create & confirm vendor-wise POs
-            for vendor in order.vendor_ids:
-                po_vals = {
-                    'partner_id': vendor.id,
-                    'origin': order.name,
-                    'order_line': [],
-                }
+            # Passed all validations
+            order.state = 'waiting'
 
-                for line in order.order_line:
-                    po_vals['order_line'].append((0, 0, {
-                        'product_id': line.product_id.id,
-                        'name': line.name,
-                        'product_qty': line.product_qty,
-                        'price_unit': line.price_unit,
-                        'product_uom_id': line.product_uom_id.id,
-                        'date_planned': line.date_planned,
+    def button_confirm(self):
+        for order in self:
+            if order.state != 'approved':
+                raise ValidationError("You cannot confirm the order until it is approved.")
+        return super(PurchaseOrder, self).button_confirm()
 
-                    }))
 
-                new_po = self.env['purchase.order'].create(po_vals)
+    def action_send_for_approval(self):
+        for order in self:
+            if not order.partner_id:
+                raise ValidationError(_("Please select a Vendor before sending for approval."))
 
-                # Confirm vendor PO
-                super(PurchaseOrder, new_po).button_confirm()
+            if not order.order_line:
+                raise ValidationError(_("Please add at least one product."))
 
-            return res
+            if order.amount_total <= 0:
+                raise ValidationError(_("Total amount must be greater than zero."))
+
+            order.state = 'to_approve'
+
+            # Internal notification
+            order.message_post(
+                body=_("Record waiting for approval.")
+            )
+
+    def action_reject(self):
+        user = self.env.user
+
+        if not (
+            user.has_group('sandy_custom_changes.group_avp_admin') or
+            user.has_group('sandy_custom_changes.group_finance')
+        ):
+            raise UserError(_("You are not authorized to reject this PO."))
+
+        self.state = 'rejected'
+
+        self.message_post(
+            body=_("Purchase Order rejected by %s.") % user.name
+        )
+
+    def _get_approval_groups_in_sequence(self):
+        return self.env['res.groups'].search(
+            [('privilege_id.name', '=', 'Purchase Approvals')],
+            order='id'
+        )
+
+    def action_approve(self):
+        user = self.env.user
+
+        if not (
+            user.has_group('sandy_custom_changes.group_avp_admin') or
+            user.has_group('sandy_custom_changes.group_finance')
+        ):
+            raise UserError(_("You are not authorized to approve this PO."))
+
+        self.state = 'approved'
+
+        # Convert to Purchase Order
+        self.button_confirm()
+
+        self.message_post(
+            body=_("Purchase Order approved by %s.") % user.name
+        )
+
+
+
+
+
+    # def button_confirm(self):
+    #     for order in self:
+
+    #         # Normal behavior if no multi-vendor selected
+    #         if not order.vendor_ids:
+    #             return super(PurchaseOrder, self).button_confirm()
+
+    #         # Prevent duplicate generation
+    #         if order.state != 'draft':
+    #             continue
+
+    #         # 1️⃣ Confirm ORIGINAL PO
+    #         res = super(PurchaseOrder, order).button_confirm()
+
+    #         # 2️⃣ Create & confirm vendor-wise POs
+    #         for vendor in order.vendor_ids:
+    #             po_vals = {
+    #                 'partner_id': vendor.id,
+    #                 'origin': order.name,
+    #                 'order_line': [],
+    #             }
+
+    #             for line in order.order_line:
+    #                 po_vals['order_line'].append((0, 0, {
+    #                     'product_id': line.product_id.id,
+    #                     'name': line.name,
+    #                     'product_qty': line.product_qty,
+    #                     'price_unit': line.price_unit,
+    #                     'product_uom_id': line.product_uom_id.id,
+    #                     'date_planned': line.date_planned,
+
+    #                 }))
+
+    #             new_po = self.env['purchase.order'].create(po_vals)
+
+    #             # Confirm vendor PO
+    #             super(PurchaseOrder, new_po).button_confirm()
+
+    #         return res
 
     
